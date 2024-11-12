d78396ec5fc71ff77fb2d53ff16cff568f3094ef
ktc-jongseok <146807248+ktc-jongseok@users.noreply.github.com>
2024-11-12 23:16:25 +0900

    add game background.

--- app/src/main/java/com/bae/composeflappy/MainActivity.kt
+++ app/src/main/java/com/bae/composeflappy/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.bae.composeflappy.ui.theme.ComposeFlappyTheme
 
@@ -44,7 +47,13 @@ fun FlappyBirdGame(
             .fillMaxSize()
             .background(Color.Cyan)
     ) {
-
+        // Background
+        Image(
+            painter = painterResource(R.drawable.flappy_background),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
